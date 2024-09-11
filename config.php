b3d5dbbde399888dd7d8320741b5e0017f369eb6
Zuby <137553853+zubyw@users.noreply.github.com>
2024-09-11 13:53:39 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,13 +1,1 @@
-<?php
 
-$host = "localhost";
-$username = "88517";
-$password = "Gymschoen17!";
-$database = "db_88517";
-
-$mysqli = mysqli_connect(
-    $host, 
-    $username, 
-    $password, 
-    $database);
-?>
